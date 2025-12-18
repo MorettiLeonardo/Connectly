159f8046e1ea603b6da97a5b22b592e6cb11c381
Leo <leonardonabarro2003@gmail.com>
2025-12-18 11:12:29 -0300

    add application project

---
+++ src/Connectly.Application/Configurations/ApiResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Connectly.Application.Configurations
+{
+    public record ApiResponse<T>
+    {
+        private readonly int _code;
+        public string Message { get; init; } = string.Empty;
+        public T? Data { get; init; }
+
+        [JsonConstructor]
+        public ApiResponse(int V) => _code = 200;
+
+        public ApiResponse(int code, string message, T? data)
+        {
+            _code = code;
+            Message = message;
+            Data = data;
+        }
+
+        [JsonIgnore]
+        public bool IsSuccess => _code is >= 200 and <= 299;
+    }
+}
